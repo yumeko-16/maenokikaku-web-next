dff6d01c50e32cc0ebaca151d977975c1539e152
tomohiro.maeno <>
2025-10-05 01:20:50 +0700

    update: Set the base style for the hero component.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,3 +1,5 @@
+import styles from './Hero.module.scss';
+
 type Props = {
   heading: string;
   lead: string;
@@ -6,12 +8,16 @@ type Props = {
 
 export default function Hero({ heading, lead, imageOn = false }: Props) {
   return (
-    <div>
-      <h1>{heading}</h1>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>{heading}</h1>
 
-      <p>{lead}</p>
+      <div className={styles.lead}>
+        <p>{lead}</p>
+      </div>
 
       {imageOn && <figure> [画像] </figure>}
+
+      <div className={styles.btn}>お問い合わせ</div>
     </div>
   );
 }
