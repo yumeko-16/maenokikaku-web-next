85ea8e2b427b3f82ec9737c31f2703235095e836
tomohiro.maeno <>
2025-10-05 13:42:20 +0700

    clean: Organize the layout of common components.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import Logo from '@/components/Logo/Logo';
 import Nav from '@/components/Nav/Nav';
+import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header>
-      <Logo />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo />
+        <Nav />
+      </div>
     </header>
   );
 }
