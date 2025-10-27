891ad9eae3dd38e3f23d4789c69e001fc8669dfb
tomohiro.maeno <>
2025-10-27 20:13:14 +0700

    update: Place the Container component inside the hero component.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,10 @@
-import Container from '@/components/Container/Container';
 import Hero from '@/components/Hero/Hero';
 
 export default function About() {
   return (
-    <Container>
-      <Hero
-        heading="私たちについて"
-        lead="当社は株式会社エクシオテック（旧 大東工業株式会社）の指定販売店として、寺院用ソフト「寺院エキスパートシステム」の販売、導入指導、サポートを行っています。"
-      />
-    </Container>
+    <Hero
+      heading="私たちについて"
+      lead="当社は株式会社エクシオテック（旧 大東工業株式会社）の指定販売店として、寺院用ソフト「寺院エキスパートシステム」の販売、導入指導、サポートを行っています。"
+    />
   );
 }
