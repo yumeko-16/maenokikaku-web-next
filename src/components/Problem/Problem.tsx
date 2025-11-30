6ac2ddfb20ad548f91c98609b1c21f325b42f3ef
tomohiro.maeno <>
2025-12-01 00:42:31 +0700

    clean: Separate data and structure.

--- src/components/Problem/Problem.tsx
+++ src/components/Problem/Problem.tsx
@@ -2,28 +2,25 @@ import Container from '@/components/Container/Container';
 import Heading from '@/components/Heading/Heading';
 import styles from './Problem.module.scss';
 
+const problems = [
+  '万一の災害で寺務に必要な書類が消失する恐れがある。',
+  '住職しか分からない情報が多く、引き継ぎに不安がある。',
+  '寺院管理ソフトは難しそうで導入に踏み切れない。',
+  '寺院管理ソフトの種類が多くて、どれを選べば良いか分からない。',
+];
+
 export default function Problem() {
   return (
     <Container>
       <Heading>寺院管理で、こんな課題に直面していませんか？</Heading>
 
       <ul className={styles.cards}>
-        <li className={styles.card}>
-          <span className={styles.avatar}></span>
-          万一の災害で寺務に必要な書類が消失する恐れがある。
-        </li>
-        <li className={styles.card}>
-          <span className={styles.avatar}></span>
-          住職しか分からない情報が多く、引き継ぎに不安がある。
-        </li>
-        <li className={styles.card}>
-          <span className={styles.avatar}></span>
-          寺院管理ソフトは難しそうで導入に踏み切れない。
-        </li>
-        <li className={styles.card}>
-          <span className={styles.avatar}></span>
-          寺院管理ソフトの種類が多くて、どれを選べば良いか分からない。
-        </li>
+        {problems.map((problem, index) => (
+          <li className={styles.card} key={index}>
+            <span className={styles.avatar}></span>
+            {problem}
+          </li>
+        ))}
       </ul>
     </Container>
   );
