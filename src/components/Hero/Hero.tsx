7d5007a87b1b324ca89bf3b4389d8220606d2581
tomohiro.maeno <>
2025-12-17 23:58:08 +0700

    update: Enable line breaks in heading text.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Image from 'next/image';
 import Container from '@/components/Container';
 import heroImg from '@/images/hero.webp';
 import styles from './Hero.module.scss';
 
 type Props = {
-  heading: string;
+  heading: string | string[];
   lead: string;
   imageOn?: boolean;
 };
@@ -15,11 +15,23 @@ type Props = {
 export default function Hero({ heading, lead, imageOn = false }: Props) {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
 
+  const headingSegments: string[] = Array.isArray(heading)
+    ? heading
+    : [heading];
+
   return (
     <Container>
       <div className={styles.gridContainer}>
         <div>
-          <h1 className={styles.heading}>{heading}</h1>
+          <h1 className={styles.heading}>
+            {headingSegments.map((text, i) => (
+              <Fragment key={i}>
+                {text}
+                {i < headingSegments.length - 1 && <wbr />}
+              </Fragment>
+            ))}
+          </h1>
+
           <div className={styles.lead}>
             <p>{lead}</p>
           </div>
