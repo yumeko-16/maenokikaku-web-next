28b87768af376ab92020bb58f009e03c7a87c7e1
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2025-12-30 16:56:48 +0700

    update: Modify the design of the contact module.

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -14,18 +14,24 @@ const data = {
   },
 };
 
+const { phone, button } = data.contents;
+
 export default function Contact() {
   return (
-    <div className={styles.contact}>
-      <div className={styles.phone}>
-        <Link className={styles.num} href={data.contents.phone.href}>
-          {data.contents.phone.num}
-        </Link>
+    <div className={styles.wrapper}>
+      <div className={styles.item}>
+        <div className={styles.heading}>お電話からのご相談はこちら</div>
+        <div className={styles.phone}>
+          <Link className={styles.num} href={phone.href}>
+            {phone.num}
+          </Link>
+        </div>
       </div>
 
-      <div className={styles.btn}>
-        <Link className={styles.link} href={data.contents.button.href}>
-          {data.contents.button.text}
+      <div className={styles.item}>
+        <div className={styles.heading}>フォームからのご相談はこちら</div>
+        <Link className={styles.link} href={button.href}>
+          {button.text}
         </Link>
       </div>
     </div>
