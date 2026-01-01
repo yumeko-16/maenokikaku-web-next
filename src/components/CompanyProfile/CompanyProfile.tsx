5489b14e316d5a656215ebf42a07682720b27006
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-01-01 21:27:38 +0700

    update: Add a type to the content data.

--- src/components/CompanyProfile/CompanyProfile.tsx
+++ src/components/CompanyProfile/CompanyProfile.tsx
@@ -1,35 +1,41 @@
 import styles from './CompanyProfile.module.scss';
 
-const data = {
+type CompanyProfileItem = {
+  id: string;
+  term: string;
+  descriptions: string[];
+};
+
+const data: { contents: CompanyProfileItem[] } = {
   contents: [
     {
-      key: 'name',
-      label: '会社名',
-      values: ['株式会社前野企画'],
+      id: 'name',
+      term: '会社名',
+      descriptions: ['株式会社前野企画'],
     },
     {
-      key: 'representative',
-      label: '代表者',
-      values: ['代表取締役社長 前野 太'],
+      id: 'representative',
+      term: '代表者',
+      descriptions: ['代表取締役社長 前野 太'],
     },
     {
-      key: 'location',
-      label: '所在地',
-      values: ['〒350-0405', '埼玉県入間郡越生町津久根672-4'],
+      id: 'location',
+      term: '所在地',
+      descriptions: ['〒350-0405', '埼玉県入間郡越生町津久根672-4'],
     },
     {
-      key: 'contact',
-      label: '連絡先',
-      values: [
+      id: 'contact',
+      term: '連絡先',
+      descriptions: [
         'FAX: 049-292-5186',
         'TEL: 090-2679-3570',
         'MAIL: maenokikaku@email.plala.or.jp',
       ],
     },
     {
-      key: 'business',
-      label: '事業内容',
-      values: [
+      id: 'business',
+      term: '事業内容',
+      descriptions: [
         '寺務支援',
         '墓地図制作',
         '過去帳和綴じ制作',
@@ -45,12 +51,12 @@ const data = {
 export default function CompanyProfile() {
   return (
     <dl className={styles.definitionlist}>
-      {data.contents.map(({ key, label, values }) => (
-        <div key={key} className={styles.unit}>
-          <dt className={styles.term}>{label}</dt>
+      {data.contents.map(({ id, term, descriptions }) => (
+        <div key={id} className={styles.unit}>
+          <dt className={styles.term}>{term}</dt>
           <dd className={styles.description}>
             <ul>
-              {values.map((value, index) => (
+              {descriptions.map((value, index) => (
                 <li key={index}>{value}</li>
               ))}
             </ul>
